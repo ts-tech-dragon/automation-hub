f5390acfba4fef416dd4ad1e3c90e63e019f1301
ts-tech-dragon <tstechupdates@gmail.com>
2026-05-12 12:43:04 +0530

    update:market aux urls updated.

--- src/tasks/market-pulse/market-aux.ts
+++ src/tasks/market-pulse/market-aux.ts
@@ -5,10 +5,16 @@ import { getTimeInIST } from "../../../lib/helpers/index.js";
 export const getMarketAuxData = async () => {
   const TODAY = getTimeInIST("YYYY-MM-DD", 1);
 
+  const urlsArr = [
+    `https://api.marketaux.com/v1/news/all?countries=in&sentiment_gte=0.1&language=en&api_token=${ENV_VARS.MARKET_AUX_API_KEY}`,
+    `https://api.marketaux.com/v1/news/all?countries=in&sentiment_lte=-0.1&language=en&api_token=${ENV_VARS.MARKET_AUX_API_KEY}`,
+    `https://api.marketaux.com/v1/news/all?countries=in&filter_entities=true&limit=3&published_after=${TODAY}T09:09&api_token=${ENV_VARS.MARKET_AUX_API_KEY}`,
+  ];
+
+  const url = urlsArr[Math.floor(Math.random() * urlsArr.length)] as string;
+
   try {
-    const { data } = await axios.get(
-      `https://api.marketaux.com/v1/news/all?countries=in&filter_entities=true&limit=3&published_after=${TODAY}T09:09&api_token=${ENV_VARS.MARKET_AUX_API_KEY}`,
-    );
+    const { data } = await axios.get(url);
 
     return data ?? { data: [] };
   } catch (error) {
