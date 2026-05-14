e78175c9c2d2012ca0895e7d34a411694e955f64
ts-tech-dragon <tstechupdates@gmail.com>
2026-05-14 22:38:49 +0530

    update:receipe prompt updated.

--- lib/constants/miscellaneous/prompt.ts
+++ lib/constants/miscellaneous/prompt.ts
@@ -7,6 +7,21 @@ Generate a random South Indian
   find random special dishes from thanjavur, karaikudi, madurai, tirunelveli,theni,kanyakumari,salem,coimbatore.
   Don't include dishes like idli, arisi paruppu satham and other dishes made typically in home.
   Include unique and lesser-known dishes that are still delicious and easy to make.
+  Also find some dishes from this cuisines "Karaikudi Chettinad",
+  "North Indian",
+  "South Indian",
+  "Indo-Chinese",
+  "Italian",
+  "Mexican",
+  "Thai",
+  "Japanese",
+  "Mediterranean",
+  "French",
+  "Middle Eastern",
+  "Vietnamese",
+  "Spanish",
+  "Korean",
+  "Greek" if possible.
   Sometimes add vareity rice like puliyodarai, lemon rice, tamarind rice, etc. but not every time.
   check youtube for inspiration.
   Format it beautifully for WhatsApp with emojis and bold text. message length within 4000 characters message
